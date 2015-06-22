7c5183c5e61b3a7c8ad0eea1109012487ad90977
gkbsoftware <gkbsoftware@gmail.com>
2015-06-22 12:26:13 -0400

    updated tweets view

--- views/tweets.jsx
+++ views/tweets.jsx
@@ -3,11 +3,26 @@ var DefaultLayout = require('./layouts/default');
 
 var Tweet = React.createClass({
   render: function() {
-    return <p>
-      <b>@{this.props.user.screen_name}</b>
-      <br />
-      {this.props.text}
-    </p>;
+    return (
+      <div className="tweet container">
+        <div className="row">
+          <div className="col-md-2 text-center">
+            <img src={this.props.user.profile_image_url} className="img-circle"/>
+            <br />
+            <b>{this.props.user.name}</b>
+            <br />
+            <a href={"https://twitter.com/"+this.props.user.screen_name}>@{this.props.user.screen_name}</a>
+          </div>
+
+          <div className="col-md-10">
+            <b>@{this.props.user.screen_name + " - "}</b>
+            <em>{this.props.created_at}</em>
+            {this.props.text}
+          </div>
+
+        </div>
+      </div>
+    )
   }
 })
 
@@ -18,6 +33,8 @@ var TweetList = React.createClass({
     });
     return (
       <DefaultLayout title={this.props.title}>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+        <h1>TweetList</h1>
         {tweets}
       </DefaultLayout>
     );
